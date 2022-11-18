fea80e0141c2ae11e346be53aceb63d4aa99f4ce
Ait Mlouk Ismail <98101279+Mlouk8988@users.noreply.github.com>
2022-11-18 17:02:10 +0100

    Update main.py

--- Scraping data/Scraping + Automated Amazon Price Tracker/main.py
+++ Scraping data/Scraping + Automated Amazon Price Tracker/main.py
@@ -6,7 +6,7 @@ import smtplib
 url = "https://www.amazon.com/dp/B08CF573K3/ref=sbl_dpx_kitchen-electric-cookware_B08GC6PL3D_0"
 header = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9,ar;q=0.8"
+    "Accept-Language": ""
 }
 
 response = requests.get(url, headers=header)
